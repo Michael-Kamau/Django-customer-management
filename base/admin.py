fc9644133e94fed91dc1283534740ba64e7282e6
Michael Kamau <kamau.karitu@gmail.com>
2024-04-28 01:54:23 +0300

    - Add models to Django admin

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Customer, CustomerBusiness, Country, County, SubCounty, Ward
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(CustomerBusiness)
+admin.site.register(Country)
+admin.site.register(County)
+admin.site.register(SubCounty)
+admin.site.register(Ward)
+
--- base/models.py
+++ base/models.py
@@ -1,14 +1,66 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class County(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class SubCounty(models.Model):
+    name = models.CharField(max_length=100)
+    county = models.ForeignKey(County, on_delete = models.CASCADE, blank = True, null = True)
+
+    def __str__(self):
+        return self.name
+
+
+class Ward(models.Model):
+    name = models.CharField(max_length=100)
+    sub_county = models.ForeignKey(SubCounty, on_delete = models.CASCADE, blank = True, null = True)
+
+    def __str__(self):
+        return self.name
+
+
+class BusinessCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=20)
     email = models.EmailField()
     date_of_birth = models.DateField()
-#     user = models.ForeignKey(User, on_delete = models.CASCADE, blank = True, null = True)
+    country = models.ForeignKey(Country, on_delete = models.CASCADE, blank = True, null = True)
 
+    def __str__(self):
+        return self.name
 
+class CustomerBusiness(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    business_category = models.ForeignKey(BusinessCategory, on_delete = models.CASCADE)
+    ward = models.ForeignKey(Ward, on_delete = models.CASCADE)
+    name = models.CharField(max_length=20)
+    business_registration_date = models.DateField()
+
+    @property
+    def age_of_business(self):
+        from datetime import date
+        return (date.today() - self.business_registration_date).days
 
     def __str__(self):
         return self.name
